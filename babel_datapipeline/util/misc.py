4cef71309966183370cefa4cf3ad8c6189d7729f
Rohan <rohanaras@gmail.com>
2016-03-14 16:22:40 -0700

    fixed potential .split issue

--- babel_datapipeline/util/misc.py
+++ babel_datapipeline/util/misc.py
@@ -1,7 +1,7 @@
 def countnodes(infile):
     distinct = set()
     for line in infile:
-        paperIDs = line.split(' ')
+        paperIDs = line.split()
         distinct.add(paperIDs[0])
         distinct.add(paperIDs[1])
     infile.seek(0)
